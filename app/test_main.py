c949f66821a587dd8d8f3c64ef34b188fc5eef69
Gonza <200260358+xshrp@users.noreply.github.com>
2026-04-07 19:18:31 -0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -13,7 +13,7 @@ class TestApp(unittest.TestCase):
 
     def test_hello_endpoint(self):
         response = self.app.get('/')
-        self.assertEqual(response.status_code, 2000)
+        self.assertEqual(response.status_code, 200)
         data = response.get_json()
         self.assertEqual(data['message'], 'Hello! This is a sample CI/CD application')
 
